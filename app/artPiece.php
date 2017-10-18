da407b63808d7165280a27a08b38e01f9ddf8e8f
Camotubi <camotubi@gmail.com>
2017-10-18 00:41:33 -0500

    add eloquent models

---
+++ app/artPiece.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class artPiece extends Model
+{
+	protected $table = "artPieces";
+}
---
+++ app/restorer.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class restorer extends Model
+{
+    protected $table = "restores";
+}
