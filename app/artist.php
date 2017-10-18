d372e3b356bc1802c7c0d4d39612cd3d369f8869
Camotubi <camotubi@gmail.com>
2017-10-18 02:19:29 -0500

    ya perdi el hilo

--- app/artist.php
+++ app/artist.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class artist extends Model
 {
-    //
+    protected $table = "artists";
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,21 +2,43 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>App Name - @yield('title')</title>
 	<link rel="stylesheet" href="/css/app.css">
-	<link rel="stylesheet" href="/css/custom.css">
+<script src="/js/app.js"></script>	
 <!--        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css"> -->
     </head>
     <body>
+<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Sinclair</a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
 {{--	@include('navbar1') --}}
 {{--	@section('sidebar') --}}
 	@show
+
 		<div class="container">
 		    @yield('content')
 		</div>
-<script src="/js/app.js"></script>	
 <!--<script src="/js/components_util.js"></script> -->
 </body>
 </html>
