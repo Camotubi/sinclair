84155572d0c185ccf89d7bf3b1ee4e6d851775d3
Camotubi <camotubi@gmail.com>
2017-10-19 20:20:08 -0500

    add a seeder for the artpieces table

---
+++ database/seeds/artPiecesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class artPiecesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    
+        public function run()
+    {
+	      DB::table('artPieces')->insert([
+            'name' => str_random(10),
+            'technique' => str_random(10),
+	    'style' => str_random(10),
+	    'currentLocation' =>str_random(10),
+	    'era'=>str_random(10),
+	    'criticalAnalisis' =>str_random(50),
+        ]);
+        //
+    
+    }
+}
