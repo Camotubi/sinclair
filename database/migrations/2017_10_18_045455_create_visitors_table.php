77db2b0ff082bfaebee43547530cb53e4b9c1111
Camotubi <camotubi@gmail.com>
2017-10-19 20:19:10 -0500

    fix naming issue not allowing migration process

--- database/migrations/2017_10_18_045455_create_visitors_table.php
+++ database/migrations/2017_10_18_045455_create_visitors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVisitorTable extends Migration
+class CreateVisitorsTable extends Migration
 {
     /**
      * Run the migrations.
