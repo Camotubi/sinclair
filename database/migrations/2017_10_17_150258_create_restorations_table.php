6d11d1aeb5e6a02e2675f182a350f970bf607908
tarowookjin <tarowj05@gmail.com>
2017-11-03 14:14:37 -0500

    Migrations

---
+++ database/migrations/2017_10_17_150258_create_restorations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRestorationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restorations', function (Blueprint $table) {
+            $table->increments('id');
+	    $table->timestamps();
+	    $table->date('restored_at');
+	    $table->text('description');
+	    $table->integer('restorer');
+	    $table->integer('artPieceId')->unsigned();
+	    $table->foreign('artPieceId')->references('id')->on('artPieces');	
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restorations');
+    }
+}
---
+++ database/migrations/2017_10_17_151527_create_people_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+	    $table->text('name');
+	    $table->text('lastname');
+	    $table->text('identification');
+	    $table->date('birthDate');
+	    $table->text('address');
+	    $table->text('phone');
+	    $table->string('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
