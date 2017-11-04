3a18d7ea6a2bd86e46a1fc44001dd376568c4521
Camotubi <camotubi@gmail.com>
2017-11-03 23:05:05 -0500

    add database seeder for artpiecetable

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,8 +9,16 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+        public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-    }
+	      DB::table('artPieces')->insert([
+            'name' => str_random(10),
+            'technique' => str_random(10),
+	    'style' => str_random(10),
+	    'currentLocation' =>str_random(10),
+	    'era'=>str_random(10),
+	    'criticalAnalisis' =>str_random(50),
+        ]);
+}
+ 
 }
