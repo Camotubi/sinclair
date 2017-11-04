ad6df8c7177644d05c5c1ca7ebbe324baffc9f1b
tarowookjin <tarowj05@gmail.com>
2017-11-03 21:43:53 -0500

    added routes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,18 @@
 Route::get('/', function () {
     return view('frontend.index');
 });
-Route::get('/home', function () {
+Route::get('/index', function () {
     return view('frontend.index');
+});
+Route::get('/quienes_somos', function () {
+    return view('frontend.quienes_somos');
+});
+Route::get('/alfredo', function () {
+    return view('frontend.alfredo');
+});
+Route::get('/galeria', function () {
+    return view('frontend.galeria');
+});
+Route::get('/contacto', function () {
+    return view('frontend.contacto');
 });
