0fbcb6f8779441541c75893c2acc9defcf9d9bca
Camotubi <camotubi@gmail.com>
2017-11-12 23:58:39 -0500

    Add artPiece factory

--- database/factories/ArtPieceFactory.php
+++ database/factories/ArtPieceFactory.php
@@ -5,6 +5,11 @@ use Faker\Generator as Faker;
 $factory->define(App\ArtPiece::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
+	'technique' => $faker->word,
+	'currentLocation' =>$faker->address,
+	'creationDate' => $faker->date,
+	'criticalAnalisis' => $faker->paragraph,
+
 
     ];
 });
