302cc4fe25b32a4ecbd2c362dc84ffa6f961e8cc
Camotubi <camotubi@gmail.com>
2017-11-13 02:04:20 -0500

    add sidbar to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,55 @@
 @include('components.navbar1')
 	@show
 
-		<div id="app" class="container">
-		    @yield('content')
+		<div id="app" class="container-fluid">
+		<div class="row">
+<nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light navbar navbar-light sidebar">
+          <ul class="nav nav-pills flex-column">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Reports</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Analytics</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Export</a>
+            </li>
+          </ul>
+
+          <ul class="nav nav-pills flex-column">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Nav item</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Nav item again</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">One more nav</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Another nav item</a>
+            </li>
+          </ul>
+
+          <ul class="nav nav-pills flex-column">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Nav item again</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">One more nav</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Another nav item</a>
+            </li>
+          </ul>
+        </nav>   
+<main class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">    
+		@yield('content')
+</main>
+		</div>
 		</div>
     <footer class="footer">
       <div class="containter">
