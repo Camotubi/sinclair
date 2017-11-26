ef2fea436878c8e0d4c212aad800df1eafac1d32
Roy Henriquez <roy.henriquez@gmail.com>
2017-11-26 03:22:50 -0500

    Agregado de las diferente entidades al sidebar

--- resources/views/components/sidebar1.blade.php
+++ resources/views/components/sidebar1.blade.php
@@ -32,6 +32,9 @@
 				<a class="nav-link" href="/Inmobilario">Ver</a>
 			</li>
 			<li class="nav-item">
+				<a class="nav-link" href="/Inmobilario/create">Registrar</a>
+			</li>
+			<li class="nav-item">
 				<a class="nav-link" href="#">Alquileres</a>
 			</li>
 			<li class="nav-item">
@@ -67,6 +70,132 @@
 			</li>
 			<li class="nav-item">
 				<a class="nav-link" href="/exhibition/create">Registrar</a>
+			</li>
+		</ul>
+	</div>
+
+	<a class="nav-fill" data-toggle="collapse" href="#collapse5" aria-expanded="false" aria-controls="collapseExample">
+		Estilos artísticos
+	</a>
+	<div id ="collapse5" class ="collapse">
+		<ul class="nav nav-pills flex-column">
+			<li class="nav-item">
+				<a class="nav-link" href="/artStyle">Ver</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/artStyle/create">Registrar</a>
+			</li>
+		</ul>
+	</div>
+
+	<a class="nav-fill" data-toggle="collapse" href="#collapse6" aria-expanded="false" aria-controls="collapseExample">
+		Condecoraciones
+	</a>
+	<div id ="collapse6" class ="collapse">
+		<ul class="nav nav-pills flex-column">
+			<li class="nav-item">
+				<a class="nav-link" href="/condecoration">Ver</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/condecoration/create">Registrar</a>
+			</li>
+		</ul>
+	</div>
+
+	<a class="nav-fill" data-toggle="collapse" href="#collapse7" aria-expanded="false" aria-controls="collapseExample">
+		Aseguradoras
+	</a>
+	<div id ="collapse7" class ="collapse">
+		<ul class="nav nav-pills flex-column">
+			<li class="nav-item">
+				<a class="nav-link" href="/insuranceCarrier">Ver</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/insuranceCarrier/create">Registrar</a>
+			</li>
+		</ul>
+	</div>
+
+	<a class="nav-fill" data-toggle="collapse" href="#collapse7" aria-expanded="false" aria-controls="collapseExample">
+		Memorabilia
+	</a>
+	<div id ="collapse7" class ="collapse">
+		<ul class="nav nav-pills flex-column">
+			<li class="nav-item">
+				<a class="nav-link" href="/multimedia">Ver</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/multimedia/create">Registrar</a>
+			</li>
+		</ul>
+	</div>
+
+	<a class="nav-fill" data-toggle="collapse" href="#collapse8" aria-expanded="false" aria-controls="collapseExample">
+		Publicaciones
+	</a>
+	<div id ="collapse8" class ="collapse">
+		<ul class="nav nav-pills flex-column">
+			<li class="nav-item">
+				<a class="nav-link" href="/publication">Ver</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/publication/create">Registrar</a>
+			</li>
+		</ul>
+	</div>
+
+	<a class="nav-fill" data-toggle="collapse" href="#collapse9" aria-expanded="false" aria-controls="collapseExample">
+		Personas Relacionadas a Sinclair
+	</a>
+	<div id ="collapse9" class ="collapse">
+		<ul class="nav nav-pills flex-column">
+			<li class="nav-item">
+				<a class="nav-link" href="/sinclairPerson">Ver</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/sinclairPerson/create">Registrar</a>
+			</li>
+		</ul>
+	</div>
+
+	<a class="nav-fill" data-toggle="collapse" href="#collapse10" aria-expanded="false" aria-controls="collapseExample">
+		Etiquetas
+	</a>
+	<div id ="collapse10" class ="collapse">
+		<ul class="nav nav-pills flex-column">
+			<li class="nav-item">
+				<a class="nav-link" href="/tag">Ver</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/tag/create">Registrar</a>
+			</li>
+		</ul>
+	</div>
+
+	<a class="nav-fill" data-toggle="collapse" href="#collapse11" aria-expanded="false" aria-controls="collapseExample">
+		Visitas
+	</a>
+	<div id ="collapse11" class ="collapse">
+		<ul class="nav nav-pills flex-column">
+			<li class="nav-item">
+				<a class="nav-link" href="/visit">Ver</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/visit/create">Registrar</a>
+			</li>
+		</ul>
+	</div>
+
+	<a class="nav-fill" data-toggle="collapse" href="#collapse12" aria-expanded="false" aria-controls="collapseExample">
+		Visitantes
+	</a>
+	<div id ="collapse12" class ="collapse">
+		<ul class="nav nav-pills flex-column">
+			<li class="nav-item">
+				<a class="nav-link" href="/visitor">Ver</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/visitor/create">Registrar</a>
 			</li>
 		</ul>
 	</div>
