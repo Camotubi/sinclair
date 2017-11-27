926053c3a3662d4e68dc016fafdc8dc0f357300e
Roy Henriquez <roy.henriquez@gmail.com>
2017-11-27 11:20:36 -0500

    Creacion de nuevas rutas
    *Se agrego la ruta de contacto de frontend que hacia falta
    
    *Se corrigio el formato para que se pudiera leer mejor
    
    *Se crearon las rutas para los respectivos showDeleteConfirmation de
    cada controlador

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,11 @@ Route::get('/', function () {
 Route::get('/index', function () {
 	return view('frontend.index');
 });
+
 Route::get('/quienes_somos', function () {
 	return view('frontend.quienes_somos');
 });
+
 Route::get('/alfredo', function () {
 	return view('frontend.alfredo');
 });
@@ -30,16 +32,20 @@ Route::get('/f/artPiece/index', 'ArtPieceController@frontIndex');
 Route::get('/f/artPiece/show/{id}', 'ArtPieceController@frontShow');
 
 Route::get('/contacto', function () {
+	return view('frontend.contacto');
 });
 
 Route::get('/cita', function () {
 	return view('frontend.cita');
 });
+
 // Staff Routes
 Route::get('/login',function() {
 	return view('auth.login');
 })->name('login');
+
 Route::post('/login','Auth\LoginController@authenticate');
+
 Route::resources([
 	'artPiece' => 'ArtPieceController',
 	'dashBoard' => 'DashboardController',
@@ -58,9 +64,45 @@ Route::resources([
 	'visit' => 'VisitController',
 	'visitor' => 'VisitorController'
 ]);
+
 Route::get('/dashboard','DashboardController@index');
+
 Route::get('/dashboard/p','DashboardController@test');
-Route::get('/test','ArtStyleController@test'); 
+
+Route::get('/test','ArtStyleController@test');
+
 Route::get('artPiece/{id}/delete','ArtPieceController@showDeleteConfirmation');
+
+Route::get('artStyle/{id}/delete','ArtStyleController@showDeleteConfirmation');
+
+Route::get('condecoration/{id}/delete','CondecorationController@showDeleteConfirmation');
+
+Route::get('exhibition/{id}/delete','ExhibitionController@showDeleteConfirmation');
+
+Route::get('furniture/{id}/delete','FurnitureController@showDeleteConfirmation');
+
+Route::get('insurance/{id}/delete','InsuranceController@showDeleteConfirmation');
+
+Route::get('insuranceCarrier/{id}/delete','InsuranceCarrierController@showDeleteConfirmation');
+
+Route::get('multimedia/{id}/delete','MultimediaController@showDeleteConfirmation');
+
+Route::get('publication/{id}/delete','PublicationController@showDeleteConfirmation');
+
+Route::get('rent/{id}/delete','RentController@showDeleteConfirmation');
+
+Route::get('restoration/artPiece/{id}/delete','RestorationController@showDeleteArtPieceConfirmation');
+
+Route::get('restoration/furniture/{id}/delete','RestorationController@showDeleteFurnitureConfirmation');
+
+Route::get('sinclairPerson/{id}/delete','SinclairPersonController@showDeleteConfirmation');
+
+Route::get('tag/{id}/delete','TagController@showDeleteConfirmation');
+
+Route::get('visit/{id}/delete','VisitController@showDeleteConfirmation');
+
+Route::get('visitor/{id}/delete','VisitorController@showDeleteConfirmation');
+
 Route::get('/artPiece/{id}/addImage' ,'ArtPieceController@addImage');
+
 Route::post('/artPiece/{id}/addImage' ,'ArtPieceController@storeImage');
