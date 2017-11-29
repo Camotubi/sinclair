0e1c2581fbe25522e67cc9d947e2d4dba4bb9e42
Roy Henriquez <roy.henriquez@gmail.com>
2017-11-29 00:42:58 -0500

    Art Style Vue Table

---
+++ resources/assets/js/components/ArtStyleTable.vue
@@ -0,0 +1,81 @@
+<template>
+	<div class="container">
+		<ul>
+			<table class="table table-bordered table-sm">
+				<thead class="thead-light">
+					<tr>
+						<th v-if="show_name">Nombre</th>
+						<th v-if="show_description">Tecnica</th>
+						<th v-if="show_links">Acciones</th>
+
+					</tr>
+				</thead>
+				<tr v-for="artStyle in laravelData.data">
+
+					<td v-if="show_name" ><a :href="'/artStyle/' +artStyle.id">{{artStyle.name}}</a></td>
+					<td v-if="show_description" v-text="artStyle.description"></td>
+					<td v-if="show_links">
+						<a :href="'/artStyle/'+artStyle.id">
+							<i class="fa fa-object-ungroup" aria-hidden="true"></i>Ver
+						</a>
+						|
+						<a :href="'/artStyle/'+artStyle.id+'/edit'">
+							<i style="color:#138496" class="fa fa-pencil" aria-hidden="true"></i>Modificar
+						</a>
+						|
+						<a :href="'/artStyle/'+artStyle.id+'/delete'">
+							<i style="color:red" class="fa fa-trash" aria-hidden="true"></i>Eliminar
+						</a>
+					</td>
+				</tr>
+			</table>
+		</ul>
+		<pagination :data="laravelData" v-on:pagination-change-page="getResults"></pagination>
+	</div>
+
+</template>
+<script>
+export default {
+	data() {
+		return {
+			// Our data object that holds the Laravel paginator data
+			laravelData: {},
+		}
+	},
+	props:{
+		show_name:{default:true},
+		show_description:{default:true},
+		page_amount:{default:10},
+		show_links:{default:true},
+
+	},
+
+
+	created() {
+		// Fetch initial results
+		this.getResults();
+	},
+
+	methods: {
+		// Our method to GET results from a Laravel endpoint
+		getResults(page) {
+			var self = this
+			if (typeof page === 'undefined') {
+				page = 1;
+			}
+
+			// Using vue-resource as an example
+			axios.get('api/artStyle/paginate/'+ self.page_amount +'?page=' + page).then(
+				function(response){
+					self.laravelData = response.data;
+				});
+		}
+	}
+}
+</script>
+<style>
+
+
+
+
+</style>
